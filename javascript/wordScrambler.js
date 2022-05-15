84b1a48ea9a775f1710fefb4b8de55619328c30e
Mardonio  Abreu <mardonio.abreu@protonmail.com>
2022-05-15 10:20:13 -0500

    Logic improved

--- javascript/wordScrambler.js
+++ javascript/wordScrambler.js
@@ -1,24 +1,25 @@
 let wordToScramble;
 let flag = true;
+let word0, word = "";
+let scrambledWordArray = [];
 
 function wordValidation() {
 
     flag = true;
-    let word0, word;
     do {
         word0 = prompt("Enter a word:");
         word = word0.toLowerCase();
 
         if (word === "") {
-            console.log("Enter a non-empty word!");
+            alert("Enter a non-empty word!");
             flag = true;
 
         } else if (!isNaN(word)) {
-            console.log("Enter a word not number!");
+            alert("Enter a word not number!");
             flag = true;
 
         } else if (!(/^[a-z]*$/.test(word))) {
-            console.log("Enter a valid word!")
+            alert("Enter a valid word!")
             flag = true;
 
         } else {
@@ -37,17 +38,22 @@ charArrayLength = charArray.length;
 let numArray = [];
 
 function randomNumber() {
-    return (Math.floor(Math.random() * charArrayLength - 1));
+    return Math.floor(Math.random() * charArrayLength);
 }
 
-for (let i = 0; i < charArrayLength; i++) {
-    numArray[i] = randomNumber();
-        for (let j = i; j >= 0; j--) {
-            while (numArray[i] === numArray[j]) {
-                numArray[i] = randomNumber();
-            }
+numArray[0] = randomNumber();
+for (let i = 1; i < charArrayLength; i++) {
+    for (let j  = i; j > 0; j--) {
+        while ((numArray[i] === numArray[j]) && (j != i)) {
+            numArray[i] = randomNumber();
         }
-        console.log(numArray[i]);
-    } 
+    }
+}
+alert(numArray);
 
+for (let i = 0; i < charArrayLength; i++) {
+    scrambledWordArray[i] = charArray[numArray[i]];
+}
 
+let scrambledWord = scrambledWordArray.join("");
+alert(scrambledWord);
